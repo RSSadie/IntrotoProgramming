78d4d364def55e85a5f8a83467fb24ab7c06fcc5
RSSadie <97475205+RSSadie@users.noreply.github.com>
2022-02-26 17:19:50 -0700

    Update code.js

--- Intro Progrm/code.js
+++ Intro Progrm/code.js
@@ -1,6 +1,23 @@
+function start(){
+    console.log("start() started");
+    document.getElementById("data").rows["seconds"].innerHTML = "Reading Data...";
+    document.getElementById("startButton").disabled = true;
+    document.getElementById("stopButton").disabled = false;
+}
+
+function stop(){
+    console.log("stop() started");
+    document.getElementById("data").rows["seconds"].innerHTML = "<td>Time Elapsed:</td><td>15 seconds</td>";
+    document.getElementById("startButton").disabled = false;
+    document.getElementById("stopButton").disabled = true;
+}
+
+
+
+
 //for the assignment, we need a timer to count down from 50 to 0 by 5 sec increments and show "blastoff at 0"
 
-function blastOffTimer(){
+/*function blastOffTimer() {
     console.log("blastOffTimer() started");
     var currTime = 50;
     var i = 5;
@@ -9,82 +26,82 @@ function blastOffTimer(){
     console.log(currTime);
     currTime = currTime - i;
 
-    setTimeout(function(){
+    setTimeout(function () {
         console.log(currTime);
         document.getElementById("blastOffText").innerHTML = currTime;
         currTime = currTime - i;
     }, timeMult);
 
-    setTimeout(function(){
+    setTimeout(function () {
         console.log(currTime);
         document.getElementById("blastOffText").innerHTML = currTime;
         currTime = currTime - i;
     }, 2 * timeMult);
 
-    setTimeout(function(){
+    setTimeout(function () {
         console.log(currTime);
         document.getElementById("blastOffText").innerHTML = currTime;
         currTime = currTime - i;
     }, 3 * timeMult);
 
-    setTimeout(function(){
+    setTimeout(function () {
         console.log(currTime);
         document.getElementById("blastOffText").innerHTML = currTime;
         currTime = currTime - i;
     }, 4 * timeMult);
 
-    setTimeout(function(){
+    setTimeout(function () {
         console.log(currTime);
         document.getElementById("blastOffText").innerHTML = currTime;
         currTime = currTime - i;
     }, 5 * timeMult);
 
-    setTimeout(function(){
+    setTimeout(function () {
         console.log(currTime);
         document.getElementById("blastOffText").innerHTML = currTime;
         currTime = currTime - i;
     }, 6 * timeMult);
 
-    setTimeout(function(){
+    setTimeout(function () {
         console.log(currTime);
         document.getElementById("blastOffText").innerHTML = currTime;
         currTime = currTime - i;
     }, 7 * timeMult);
 
-    setTimeout(function(){
+    setTimeout(function () {
         console.log(currTime);
         document.getElementById("blastOffText").innerHTML = currTime;
         currTime = currTime - i;
     }, 8 * timeMult);
 
-    setTimeout(function(){
+    setTimeout(function () {
         console.log(currTime);
         document.getElementById("blastOffText").innerHTML = currTime;
         currTime = currTime - i;
     }, 9 * timeMult);
 
-    setTimeout(function(){
+    setTimeout(function () {
         console.log(currTime);
         document.getElementById("blastOffText").innerHTML = "Blastoff!!";
         currTime = currTime - i;
     }, 10 * timeMult);
 }
-
-function betterCtDown(){
+*
+function betterCtDown() {
     console.log("testCtDown() started");
     //variable to track time
     currTime = 50
     //for loop to save typing
-    for(var i = 0; i <= 10; i++){
+    for (var i = 0; i <= 10; i++) {
         console.log(i);
-        setTimeout(function(){
-            if(currTime == 0){
+        setTimeout(function () {
+            if (currTime == 0) {
                 //if we are done and need blastoff
                 document.getElementById("blastOffText").innerHTML = "Blastoff!"
-            }else if (currTime < 25){
+            } else if (currTime < 25) {
                 //because the admiral wants this
-                document.getElementById("blastOffText").innerHTML = 
-                "Warning: Less than 1/2 way to launch, time left = " + currTime
+                document.getElementById("blastOffText").innerHTML =
+                    "Warning: Less than 1/2 way to launch, time left = " + currTime
             } else {
                 //everything else
                 document.getElementById("blastOffText").innerHTML = currTime;
@@ -96,15 +113,15 @@ function betterCtDown(){
     }
 }
 
-function test(){
+function test() {
     console.log("test() started");
     var i = 0;
     currTime = 50;
-    while(i <=10){
+    while (i <= 10) {
         console.log(i);
-        setTimeout(function(){
+        setTimeout(function () {
             document.getElementById("blastOffText").innerHTML = currTime;
-            if(currTime == 0){
+            if (currTime == 0) {
                 document.getElementById("blastOffText").innerHTML = "Blastoff"
             }
             currTime = currTime - 5;
@@ -118,38 +135,39 @@ var numWins = 0;
 var numLost = 0;
 var numTies = 0;
 //Display in log "playCraps() started" when running the function playCraps()
-function playCraps(){
-console.log("playCraps() started");
-//Pick a random number, multiply by 6 and store in die1
-var die1 = Math.ceil(6 * Math.random());
-//Pick a random number, multiply by 6 and store in die2
-var die2 = Math.ceil(6 * Math.random());
-//add die1 and die2 and store in sum
-var sum = die1 + die2;
-//Display die1 on the browser at the die1Res position
-document.getElementById("die1Res").innerHTML = die1;
-//Display die2 on the browser at the die2Res position
-document.getElementById("die2Res").innerHTML = die2;
-//Display sum on the browser at the sumRes position
-document.getElementById("sumRes").innerHTML = sum;
-//If the sum is equal to 7 or 11, display "Craps, you lose!"
-if(sum == 7 || sum == 11){
-    document.getElementById("CrapsRes").innerHTML = "Craps, you lose!";
-    //Add 1 to numLost every time conditions are met
-    numLost = numLost + 1;
-    //If die1 is equal to die2, display "Doubles, you win!"
-} else if(die1 == die2 && die1 % 2 == 0){
-    document.getElementById("CrapsRes").innerHTML = "Doubles, you win!";
-    //Add 1 to numWins every time the conditions are met
-    numWins = numWins + 1;
-    //If the dice are anything else, display "Oops, it's a tie!"
-} else {
-     document.getElementById("CrapsRes").innerHTML = "Oops, it's a tie!";
-     //Add 1 to numTies every time conditions are met
-     numTies = numTies + 1;
+function playCraps() {
+    console.log("playCraps() started");
+    //Pick a random number, multiply by 6 and store in die1
+    var die1 = Math.ceil(6 * Math.random());
+    //Pick a random number, multiply by 6 and store in die2
+    var die2 = Math.ceil(6 * Math.random());
+    //add die1 and die2 and store in sum
+    var sum = die1 + die2;
+    //Display die1 on the browser at the die1Res position
+    document.getElementById("die1Res").innerHTML = die1;
+    //Display die2 on the browser at the die2Res position
+    document.getElementById("die2Res").innerHTML = die2;
+    //Display sum on the browser at the sumRes position
+    document.getElementById("sumRes").innerHTML = sum;
+    //If the sum is equal to 7 or 11, display "Craps, you lose!"
+    if (sum == 7 || sum == 11) {
+        document.getElementById("CrapsRes").innerHTML = "Craps, you lose!";
+        //Add 1 to numLost every time conditions are met
+        numLost = numLost + 1;
+        //If die1 is equal to die2, display "Doubles, you win!"
+    } else if (die1 == die2 && die1 % 2 == 0) {
+        document.getElementById("CrapsRes").innerHTML = "Doubles, you win!";
+        //Add 1 to numWins every time the conditions are met
+        numWins = numWins + 1;
+        //If the dice are anything else, display "Oops, it's a tie!"
+    } else {
+        document.getElementById("CrapsRes").innerHTML = "Oops, it's a tie!";
+        //Add 1 to numTies every time conditions are met
+        numTies = numTies + 1;
+    }
+    //Scoreboard: displays amount of wins, losses, and ties
+    document.getElementById("wins").innerHTML = numWins;
+    document.getElementById("losses").innerHTML = numLost;
+    document.getElementById("ties").innerHTML = numTies;
 }
-//Scoreboard: displays amount of wins, losses, and ties
-document.getElementById("wins").innerHTML = numWins;
-document.getElementById("losses").innerHTML = numLost;
-document.getElementById("ties").innerHTML = numTies;
-}
+*/
